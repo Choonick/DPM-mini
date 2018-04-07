5d8a28b3353ca8b66d51df590736f408b99d2e28
Choonick <wecube.1@gmail.com>
2018-04-07 16:08:00 +0900

    main 하는중

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="main">
-      main
+      <h1 class="title">Weather</h1>
+      <p class="date">7 apr. 2018 2018년 10월 1일</p>
+      <p class="description">오늘 날씨 개좋음</p>
+      <div>
+        <!-- <div>20 &#8451;</div> -->
+        <div class="description2">
+
+          <div class="weather">
+            <div class="temperature">
+              20 <sup>o</sup>
+            </div>
+            <div>
+              
+            </div>
+          </div>
+
+          <div class="dust">
+            미세먼지
+          </div>
+
+        </div>
+        
+      </div>
   </div>
 </template>
 
 <style scoped>
-  div.main{
-    background: skyblue;
-  }
-
+div.main {
+  background: skyblue;
+}
 </style>
